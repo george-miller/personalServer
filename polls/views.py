c0a6705b50f5f5fb41e0b0afb8cf9f19f5964ce5
George Miller <george@live2serve.home>
2015-07-09 23:12:24 -0400

    committing first version of my django site

---
+++ polls/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+	return HttpResponse("Hello World, this is the polls index.")
+
+def send(request):
+	mylog.lg
+
+# Create your views here.
