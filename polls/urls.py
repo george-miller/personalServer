d7a77499962ef8dd465a4a5f8faba2c6d673634e
George Miller <george@live2serve.home>
2015-07-23 21:40:15 -0400

    Completed polls app, with jQuery ajax and jquery input fields

--- polls/urls.py
+++ polls/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.IndexView.as_view(), name = 'index'),
-	url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+	url(r'^$', views.index, name = 'index'),
+	url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
 	url(r'(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 	url(r'(?P<question_id>[0-9]+)/addChoice/$', views.addChoice, name='addChoice'),
 	url(r'^addQuestion/$', views.addQuestion, name='addQuestion')
--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, render_to_response
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import RequestContext, loader
 import logging
@@ -6,69 +6,61 @@ from .models import Question, Choice, ipAlreadyVoted
 from django.views import generic
 from django.core.urlresolvers import reverse
 from django.utils import timezone
+from django.views.decorators.csrf import csrf_exempt
 
 logger = logging.getLogger(__name__)
 def get_client_ip(request):
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-    if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]
-    else:
-        ip = request.META.get('REMOTE_ADDR')
-    return ip
+	x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+	if x_forwarded_for:
+		ip = x_forwarded_for.split(',')[0]
+	else:
+		ip = request.META.get('REMOTE_ADDR')
+	return ip
 
-class IndexView(generic.ListView):
-    template_name = 'polls/index.html'
-    context_object_name = 'latest_question_list'
+def index(request):
+	return render(request, 'polls/index.html', {'latest_question_list': Question.objects.all()})
 
-    def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.filter(
-			pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
-
-
-class DetailView(generic.DetailView):
-    model = Question
-    template_name = 'polls/detail.html'
-	
-    def get_queryset(self):
-		return Question.objects.filter(pub_date__lte=timezone.now())
-
+def detail(request, question_id):
+	return render(request, 'polls/detail.html', {'question': Question.objects.get(pk=question_id)})
 
 
 def vote(request, question_id):
-    p = get_object_or_404(Question, pk=question_id)
-    try:
-        selected_choice = p.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
-        return render(request, 'polls/detail.html', {
-            'question': p,
-            'error_message': "You didn't select a choice.",
-        })
-    else:
-		logger.error(p.ipalreadyvoted_set.filter(ip=get_client_ip(request)).count())
-		if p.ipalreadyvoted_set.filter(ip=get_client_ip(request)).count() == 0:
-			selected_choice.votes += 1
-			p.ipalreadyvoted_set.add(ipAlreadyVoted(ip=get_client_ip(request)))
-			selected_choice.save()
-			p.save()
-			return HttpResponseRedirect(reverse('polls:detail', args=(p.id,)))
-		else:
-			return render(request, 'polls/detail.html', {
-				'question': p,
-				'error_message': "You already voted"})
+	p = get_object_or_404(Question, pk=question_id)
+	logger.error(p);
+	try:
+		selected_choice = p.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		# Redisplay the question voting form.
+		return HttpResponse("You didn't select a choice.")
+	else:
+		logger.error(selected_choice);
+	if p.ipalreadyvoted_set.filter(ip=get_client_ip(request)).count() == 0:
+		selected_choice.votes += 1
+		p.ipalreadyvoted_set.add(ipAlreadyVoted(ip=get_client_ip(request)))
+		selected_choice.save()
+		p.save()
+		return HttpResponseRedirect(reverse('polls:detail', args=(p.id,)))
+	else:
+		return HttpResponse("You already voted")
 
 def addChoice(request, question_id):
 	question = get_object_or_404(Question, pk=question_id)
+	for choice in question.choice_set.all():
+		if choice.choice_text == request.POST['newChoice']:
+			return HttpResponse("Choice already exists")
+			
 	newChoice = Choice()
 	newChoice.choice_text = request.POST['newChoice']
 	question.choice_set.add(newChoice)
 	question.save()
-	return HttpResponseRedirect(reverse('polls:detail', kwargs=({'pk':question.pk})))
+	return HttpResponseRedirect(reverse('polls:detail', args=(question.id,)))
 
 
 def addQuestion(request):
-	newQuestion = Question(question_text=request.POST['newQuestion'], pub_date=timezone.now())
-	newQuestion.question_text = request.POST['newQuestion']
+	for question in Question.objects.all():
+		if question.question_text == request.POST.get('newQuestion', False):
+			return HttpResponse("Question already exists");
+	
+	newQuestion = Question(question_text=request.POST.get('newQuestion', False), pub_date=timezone.now())
 	newQuestion.save()
 	return HttpResponseRedirect(reverse('polls:index'))
